9d5973d0f98af443cfe2e8fda712da2c446ffcdc
Rakshit Sharma <kumarrakshit0402@gmail.com>
2026-08-01 14:19:20 +0530

    feat(employee): add department-designation set_query, full_name preview, and status indicator

--- employee_hub/employee_hub/doctype/employee/employee.js
+++ employee_hub/employee_hub/doctype/employee/employee.js
@@ -1,8 +1,53 @@
 // Copyright (c) 2026, Rakshit Sharma and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Employee", {
-// 	refresh(frm) {
+frappe.ui.form.on("Employee", {
+	refresh(frm) {
+		set_designation_filter(frm);
+		set_status_indicator(frm);
+		add_create_leave_request_button(frm);
+		add_view_skills_summary_button(frm);
+	},
 
-// 	},
-// });
+	department(frm) {
+		frm.set_value("designation", "");
+		set_designation_filter(frm);
+	},
+
+	first_name(frm) {
+		update_full_name_preview(frm);
+	},
+
+	last_name(frm) {
+		update_full_name_preview(frm);
+	},
+});
+
+function set_designation_filter(frm) {
+	frm.set_query("designation", () => {
+		return {
+			filters: {
+				department: frm.doc.department,
+			},
+		};
+	});
+}
+
+function update_full_name_preview(frm) {
+	const first = frm.doc.first_name || "";
+	const last = frm.doc.last_name || "";
+	frm.set_value("full_name", `${first} ${last}`.trim());
+}
+
+function set_status_indicator(frm) {
+	if (frm.doc.__islocal) return;
+
+	const status_colors = {
+		Active: "green",
+		"On Leave": "orange",
+		Terminated: "red",
+		Inactive: "gray",
+	};
+	const color = status_colors[frm.doc.employee_status] || "gray";
+	frm.dashboard.add_indicator(frm.doc.employee_status, color);
+}
