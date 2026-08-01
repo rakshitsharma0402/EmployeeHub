35f99b507a9417a52f63df06e41e0906ecc15769
Rakshit Sharma <kumarrakshit0402@gmail.com>
2026-08-01 16:22:35 +0530

    chore(fixtures): export roles and workflow

--- employee_hub/hooks.py
+++ employee_hub/hooks.py
@@ -276,3 +276,8 @@ require_type_annotated_api_methods = True
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
 
+# Fixtures
+fixtures = [
+	{"dt": "Role", "filters": [["role_name", "in", ["HR Admin", "Employee"]]]},
+	{"dt": "Workflow", "filters": [["name", "=", "Leave Approval Workflow"]]},
+]
